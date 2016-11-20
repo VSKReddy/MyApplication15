65881e81aa2e44506a7c0c021595e300f0ea245e
VSKReddy <v.sathish4085@gmail.com>
2016-11-20 20:41:45 +0530

    modify the application by using webview.add testjavafile.java

--- app/src/main/java/com/techpalle/myapplication15/MainActivity.java
+++ app/src/main/java/com/techpalle/myapplication15/MainActivity.java
@@ -2,12 +2,22 @@ package com.techpalle.myapplication15;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+            webView = (WebView) findViewById(R.id.webview1);
+
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.setWebViewClient(new WebViewClient());
+            webView.loadUrl("http://Skillgun.com");
     }
 }
---
+++ app/src/main/java/com/techpalle/myapplication15/testaddfile.java
@@ -0,0 +1,8 @@
+package com.techpalle.myapplication15;
+
+/**
+ * Created by Sathish Kumar on 20-Nov-16.
+ */
+
+public class testaddfile {
+}
